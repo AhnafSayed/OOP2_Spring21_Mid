e869a5fdf4348a93410762380d7199804a6afbf5
Ahnaf Sayed <ahnafsayedtashri@gmail.com>
2021-02-14 15:29:40 +0600

    All array problems done!

---
+++ Array Problem 3/Array Problem 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 7, 7, 8, 8, 9, 9, 9, 10, 10 };
+            HashSet<int> a = new HashSet<int>();
+            for (int i = 0; i < 15; i++)
+            {
+                a.Add(arr[i]);
+            }
+            foreach (int j in a)
+            {
+                Console.WriteLine("Unique elements are: " +j);
+            }
+        }
+    }
+}
