399f411e7b90ae50d85c701fa23c8d94ee1194c1
Ahnaf Sayed <ahnafsayedtashri@gmail.com>
2021-02-14 13:55:15 +0600

    Array Problems

---
+++ Array Problem 1/Array Problem 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] b = new int[10];
+            int i;
+
+            for (i = 0; i < 10; i++)
+            {
+                b[i] = a[i];
+            }
+            Console.Write("The first array elements are :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", a[i]);
+            }
+            Console.Write("\nThe copy second array elements are :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", b[i]);
+            }
+        }
+    }
+}
