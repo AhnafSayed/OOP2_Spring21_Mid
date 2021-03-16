e5518bc28bd42f2fd4c0cc471294855c494f0f6a
Ahnaf Sayed <ahnafsayedtashri@gmail.com>
2021-03-16 11:59:32 +0600

    Account

---
+++ Account/Account/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new SavingsAccount("Avoid Rafa", "54367", 46000);
+            Account a2 = new CurrentAccount("Avoid Rafa", "56427", 46000);
+            Account a3 = new SavingsAccount("Avoid Rafa", "52866", 46000);
+
+            Person p = new Person("Avoid Rafa");
+
+            p.AddAccount(a1, a2, a3);
+            p.ShowAccontDetails();
+
+            var acc = p.GetAccount("56427");
+
+            acc.Withdraw(42000);
+
+        }
+    }
+}
---
+++ Nothing/Nothing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nothing
+{
+    class Program
+    {  static void main(string[] args)
+        {
+            int n = 1;
+            method(n);
+            Console.WriteLine(n);
+            method1(ref n);
+            Console.WriteLine(n);
+        }
+        static void method(int num)
+        {
+            num += 20;
+            Console.WriteLine(num);
+        }
+        static void method1(ref int num)
+        {
+            num += 20;
+            Console.WriteLine(num);
+        }
+    }
+}
